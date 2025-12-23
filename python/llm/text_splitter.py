e7044bfad0bb174da10525bbc7887c263882b3eb
RaoThotakura <raothotakura@hotmail.com>
2025-12-23 17:52:34 -0500

    35: First Agent - remove pycache

---
+++ python/llm/text_splitter.py
@@ -0,0 +1,14 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,  # chunk size (characters)
+    chunk_overlap=200,  # chunk overlap (characters)
+    add_start_index=True,  # track index in original document
+)
+all_splits = text_splitter.split_documents(docs)
+
+document_ids = vector_store.add_documents(documents=all_splits)
+
+print(document_ids[:3])
+
+print(f"Split blog post into {len(all_splits)} sub-documents.")
