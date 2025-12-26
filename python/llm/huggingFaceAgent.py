08fb6c66684b9bd499f1a3618c30ac88f5624fbb
RaoThotakura <raothotakura@hotmail.com>
2025-12-25 19:21:26 -0500

    42: ChatHuggingFace API Usage

--- python/llm/huggingFaceAgent.py
+++ python/llm/huggingFaceAgent.py
@@ -1,16 +1,21 @@
+# HuggingFaceChatModel is locally accessible by means of a API
+# No pricing or cost involved
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from langchain.messages import (
     HumanMessage,
     SystemMessage,
 )
+from langchain.tools import tool
+
 import os
+from pydantic import BaseModel, Field
 
 os.getenv("HUGGINGFACEHUB_API_KEY")
 
 # from huggingface_hub import login
 # login()  # You will be prompted for your HF key, which will then be saved locally
 
-llm = HuggingFaceEndpoint(
+model = HuggingFaceEndpoint(
     repo_id="deepseek-ai/DeepSeek-R1-0528",
     task="text-generation",
     max_new_tokens=512,
@@ -19,9 +24,9 @@ llm = HuggingFaceEndpoint(
     provider="auto",  # let Hugging Face choose the best provider for you
 )
 
-chat_model = ChatHuggingFace(llm=llm)
+chat_model = ChatHuggingFace(llm=model)
 
-
+# OpenAI Message prompts format
 
 messages = [
     SystemMessage(content="You're a helpful assistant"),
@@ -29,6 +34,55 @@ messages = [
         content="What happens when an unstoppable force meets an immovable object?"
     ),
 ]
-
 ai_msg = chat_model.invoke(messages)
 print(ai_msg.content)
+
+# OpenAI Completions / Dictionary format
+conversation = [
+    {"role": "system", "content": "You are a helpful assistant that translates English to French."},
+    {"role": "user", "content": "Translate: I love programming."},
+    {"role": "assistant", "content": "J'adore la programmation."},
+    {"role": "user", "content": "Translate: I love building applications."}
+]
+response = chat_model.invoke(conversation)
+print(f'AI Message: ', response)  # AIMessage("J'adore créer des applications.")
+print(f'Type of Message: ', type(response))
+
+# Tool calls
+
+@tool
+def get_weather(location: str) -> str:
+    """Get the weather at a location."""
+    return f"It's sunny in {location}."
+
+model_with_tools = chat_model.bind_tools([get_weather])
+
+response = model_with_tools.invoke("What's the weather like in Boston?")
+for tool_call in response.tool_calls:
+    # View tool calls made by the model
+    print(f"Tool: {tool_call['name']}")
+    print(f"Args: {tool_call['args']}")
+print(f"Response: ",response)
+
+class GetPopulation(BaseModel):
+    '''Get the current population in a given location'''
+    location: str = Field(..., description="The city and state, e.g. San Francisco, CA")
+
+chat_with_tools = chat_model.bind_tools([GetPopulation])
+ai_msg = chat_with_tools.invoke("Which city is hotter today and which is bigger: LA or NY?")
+response_tool_calls= ai_msg.tool_calls
+print(f'response_tool_calls: ', response_tool_calls)
+chat_model_with_tools = chat_model.bind_tools([get_weather, GetPopulation])
+ai_msg_with_tools = chat_model_with_tools.invoke("Which city is hotter today and which is bigger: Chadds Ford, PA or Pittsburgh, PA?")
+# ai_msg.tool_calls
+print(f'AI Message with tool calls: ',ai_msg_with_tools.content)
+
+for tool_call in ai_msg_with_tools.tool_calls:
+    print(f"Tool: {tool_call['name']}")
+    print(f"Args: {tool_call['args']}")
+    print(f"ID: {tool_call['id']}")
+
+# Bind (potentially multiple) tools to the model
+model_with_tools = chat_model.bind_tools([get_weather])
+
+
