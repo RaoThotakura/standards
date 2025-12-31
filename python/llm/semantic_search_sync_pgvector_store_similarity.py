f638f2da94973b3cbc3fa5d2fef0065e5fb9652f
RaoThotakura <raothotakura@hotmail.com>
2025-12-30 21:54:37 -0500

    55: Semantic Search Process; sync; async  PGVectorStore usage

--- python/llm/semantic_search_sync_pgvector_store_similarity.py
+++ python/llm/semantic_search_sync_pgvector_store_similarity.py
@@ -1,4 +1,5 @@
-import asyncio, os
+import asyncio
+import os
 
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
@@ -6,8 +7,6 @@ from langchain_ollama import OllamaEmbeddings
 from langchain_postgres import PGVector, PGEngine, PGVectorStore
 from sqlalchemy.exc import ProgrammingError
 
-from sqlalchemy.ext.asyncio import create_async_engine
-from langchain_postgres import Column
 from langchain_cohere import CohereEmbeddings
 from dotenv import load_dotenv
 load_dotenv()
@@ -62,62 +61,59 @@ print(vector_1[:10])
 # requires docker desktop running on the MacOS
 
 collection_name = "my_docs"
-
+# @title Set your values or use the defaults to connect to Docker { display-mode: "form" }
 POSTGRES_USER = "langchain"  # @param {type: "string"}
 POSTGRES_PASSWORD = "langchain"  # @param {type: "string"}
 POSTGRES_HOST = "localhost"  # @param {type: "string"}
 POSTGRES_PORT = "6024"  # @param {type: "string"}
 POSTGRES_DB = "langchain"  # @param {type: "string"}
-TABLE_NAME = "custom_test_vectorstore"  # @param {type: "string"}
+TABLE_NAME = "custom_x_vectorstore"  # @param {type: "string"}
 VECTOR_SIZE = 1024  # @param {type: "int"}
-
-# See docker command  to launch a Postgres instance with pgvector enabled. This is for asynchronous
-# docker CLI:
+# synchronous
 CONNECTION_STRING = (
-    f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}"
+    f"postgresql+psycopg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}"
     f":{POSTGRES_PORT}/{POSTGRES_DB}"
 )
 
-# Create an SQLAlchemy based Asynchronous Engine
-engine = create_async_engine(
-    CONNECTION_STRING
+# To use psycopg3 driver, set your connection string to `postgresql+psycopg://`
+# synchronous
+pg_engine = PGEngine.from_connection_string(
+    url=CONNECTION_STRING
 )
-pg_engine = PGEngine.from_engine(engine=engine)
-SCHEMA_NAME="public"
 
-# asynchronous
-async def setup_pgvector_store():
-# def setup_pgvector_store():
+vector_store = PGVectorStore.create_sync(
+    engine=pg_engine,
+    table_name='test_similarity_search_table',
+    embedding_service=embeddings
+)
+SCHEMA_NAME="public"
+embedding = CohereEmbeddings(model="embed-english-v3.0")
+#
+# synchronous
+def setup_pgvector_store():
     # Await the function call to get the result and assign it to a variable
     # asynchronous
     try:
-        await pg_engine.ainit_vectorstore_table(
+        pg_engine.init_vectorstore_table(
             table_name=TABLE_NAME,
             vector_size=VECTOR_SIZE,
             schema_name=SCHEMA_NAME,    # Default: "public"
-            id_column=Column(name="langchain_id", data_type="INTEGER"),
-            metadata_columns=[Column("len", "INTEGER")]
         )
-    except ProgrammingError as pe:
+    except ProgrammingError as e:
         await pg_engine.adrop_table(TABLE_NAME)
-        print(f"Table already exists. Skipping creation. {pe=}, {type(pe)=}")
-    # asynchronous
-    try:
-        custom_vector_store = await PGVectorStore.create(
-            engine=pg_engine,
-            table_name=TABLE_NAME,
-            schema_name=SCHEMA_NAME,
-            embedding_service=embeddings,
-            metadata_columns=["len"],
-        )
-    except Exception as rte:
-        print(f"RuntimeError while creating vector store {rte=}, {type(rte)=}")
+        print("Table already exists. Skipping creation.")
+    embedding = CohereEmbeddings(model="embed-english-v3.0")
+    # synchronous
+    custom_vector_store = PGVectorStore.create(
+        engine=pg_engine,
+        table_name=TABLE_NAME,
+        schema_name=SCHEMA_NAME,
+        embedding_service=embedding,
+        metadata_columns=["len"],
+    )
 
     print(f"Received: {custom_vector_store}")
-    try:
-        ids = await custom_vector_store.aadd_documents(documents=all_splits)
-    except Exception as exc:
-        print(f"RuntimeError while adding documents into vector store {exc=}, {type(exc)=}")
+    ids = custom_vector_store.add_documents(documents=all_splits)
 
     results = custom_vector_store.similarity_search(queries['1'])
     print(f"Results from similar text - synchronous {results[0]}")
@@ -141,8 +137,7 @@ async def setup_pgvector_store():
         print(f"Results from similar text - asynchronous {results[0]}")
     asyncio.run(similarity(queries['2']))
 
+
 # Run the entry point coroutine in the event loop
 if __name__ == "__main__":
-    asyncio.run(setup_pgvector_store())
-
-
+    setup_pgvector_store()
